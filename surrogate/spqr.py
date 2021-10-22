4ee1bc61ba40017c19b92af536fcdb4c18ee9b7a
Fabio Carrara <fabio.carrara@isti.cnr.it>
2021-10-22 10:55:44 +0200

    updated SPQR to base class

--- surrogate/spqr.py
+++ surrogate/spqr.py
@@ -3,11 +3,12 @@ import math
 import faiss
 from joblib import cpu_count, delayed, Parallel
 import numpy as np
-from scipy.sparse import csr_matrix, vstack
+from scipy import sparse
 from scipy.spatial.distance import cdist
 
 from surrogate.str_index import SurrogateTextIndex
-# from utils import parallel_cdist as cdist
+import utils
+
 
 def _spqr_encode(
     x,             # features to encode
@@ -29,21 +30,31 @@ def _spqr_encode(
     fine_distances = np.array([ cdist(r, c, metric='sqeuclidean')
             for r, c in zip(residuals, l2_centroids) ])  # m x n x f
 
-    # find top-k NNs for each subvector:
-    # this sorts the entire array (inefficient), but needed if k == f ...
-    if k == f:
-        topk = fine_distances.argsort(axis=2)[:, :, :k]  # m x n x k
-    else:
-    # ... while this partitions the array and sorts only the topk
-        unsorted_topk = fine_distances.argpartition(k, axis=2)[:, :, :k]
-        sorted_topk_idx = np.take_along_axis(fine_distances, unsorted_topk, axis=2).argsort(axis=2)
-        topk = np.take_along_axis(unsorted_topk, sorted_topk_idx, axis=2)  # m x n x k
+    # find kNNs for each subvector
+    nns = utils.bottomk_sorted(fine_distances, k, axis=2)
 
     # build the representation
+    # the nested-for-loops way ...
+    """
+    rows, cols, data = [], [], []
+    for subvector_index, sub_nns in enumerate(nns):  # 0 ...  m-1
+        for sample_index, sample_sub_nns in enumerate(sub_nns):  # 0 ... n
+            for importance, nn in enumerate(sample_sub_nns[::-1], start=1):  # 1 ... k
+                rows.append(sample_index)
+                cols.append(nn + self.f * subvector_index + self.f * self.m * coarse_codes[sample_index])
+                data.append(importance)
+
+    rows = np.array(rows)
+    cols = np.array(cols)
+    data = np.array(data)
+    
+    sparse_repr = csr_matrix((data, (rows, cols)), shape=(n, self.vocab_size))
+    """
+    # ... or the vectorized way (WARNING: heavy broadcasting ahead)
     rows = np.arange(n).reshape(1, -1, 1)
     m_shifts = np.arange(m).reshape(-1, 1, 1)
     c_shifts = coarse_codes.reshape(1, -1, 1)
-    cols = topk + f * m_shifts + f * m * c_shifts
+    cols = nns + f * m_shifts + f * m * c_shifts
     data = np.arange(k, 0, -1).reshape(1, 1, -1)
     
     rows, cols, data = np.broadcast_arrays(rows, cols, data)
@@ -64,7 +75,7 @@ class SPQR(SurrogateTextIndex):
         n_coarse_centroids=None,
         n_subvectors=None,
         n_fine_centroids_log2=None,
-        parallel=False
+        parallel=True
     ):
         """ Constructor.
         Args:
@@ -92,10 +103,10 @@ class SPQR(SurrogateTextIndex):
         ), "You must specify a prebuilt faiss index " \
             "or all the params (as ints) needed to build the index."
 
-        self.parallel = parallel
         self.l1_centroids = None
         self.l2_centroids = None
         self.db = None
+        self.permutation_length = None
 
         if isinstance(d_or_index, int):
             self.d = d_or_index
@@ -114,9 +125,9 @@ class SPQR(SurrogateTextIndex):
             self.f_log2 = int(math.log2(d_or_index.pq.ksub))
             self.f = d_or_index.pq.ksub
         
-        self.vocab_size = self.c * self.m * self.f
         self._load_centroids()
-        self.reset()
+        vocab_size = self.c * self.m * self.f
+        super().__init__(vocab_size, parallel)
 
     def _load_centroids(self):
         if self.faiss_index.is_trained:
@@ -127,104 +138,58 @@ class SPQR(SurrogateTextIndex):
             self.l2_centroids = faiss.vector_to_array(index.pq.centroids)
             # there are ksub centroids of length dsub for each of the M subvectors
             self.l2_centroids = self.l2_centroids.reshape(index.pq.M, index.pq.ksub, index.pq.dsub)
-        else:
-            self.l1_centroids = None
-            self.l2_centroids = None
 
     def train(self, x):
+        self.faiss_index.do_polysemous_training = False
         self.faiss_index.train(x)
         self._load_centroids()
     
-    def encode(self, *args, **kwargs):
-        if self.parallel:
-            return self.encode_parallel(*args, **kwargs)
-        
-        return self.encode_sequential(*args, **kwargs)
-
-    def encode_sequential(self, x, permutation_length=None):
+    def encode(self, x, inverted=True):
         """ Encodes vectors and returns their SPQR representations.
         Args:
             x (ndarray): a (N,D)-shaped matrix of vectors to be encoded.
-            permutation_length (int): the number of nearest fine centroids to keep
-                                      when truncating the permutation; if None, the
-                                      whole permutation is used. Defaults to None.
         """
         assert self.faiss_index.is_trained, "Index must be trained before encoding."
+        assert self.d == x.shape[1], f"Dimension mismatch: expected {self.d}, got {x.shape[1]}"
 
-        k = self.f if permutation_length is None else permutation_length
-        n, d = x.shape
-        
-        assert self.d == d, f"Dimension mismatch: expected {self.d}, got {d}"
+        k = self.f if self.permutation_length is None else self.permutation_length
 
-        # find nearest coarse centroids
-        coarse_codes = cdist(x, self.l1_centroids, metric='sqeuclidean').argmin(axis=1) # n 
+        if self.parallel:
+            batch_size = int(math.ceil(len(x) / cpu_count()))
+            results = Parallel(n_jobs=-1, prefer='threads', require='sharedmem')(
+                delayed(_spqr_encode)(x[i:i+batch_size], self.l1_centroids, self.l2_centroids, k)
+                for i in range(0, len(x), batch_size)
+            )
 
-        # compute residual
-        residuals = x - self.l1_centroids[coarse_codes]  # n x d
-        residuals = np.split(residuals, self.m, axis=1)  # m x n x d/m
-
-        # compute distances to fine centroids for each subvector
-        fine_distances = np.array([ cdist(r, c, metric='sqeuclidean')
-                for r, c in zip(residuals, self.l2_centroids) ])  # m x n x f
-
-        # find top-k NNs for each subvector:
-        # this sorts the entire array (inefficient), but needed if k == f ...
-        if k == self.f:
-            topk = fine_distances.argsort(axis=2)[:, :, :k]  # m x n x k
+            # use COO for fast sparse matrix concatenation
+            results = [sparse.coo_matrix((data, (rows, cols)), shape=(n, self.vocab_size)) for rows, cols, data, n in results]
+            results = sparse.vstack(results)
+            results = results.T if inverted else results
+            # then convert to CSR
+            results = results.tocsr()
+            return results
+       
+        rows, cols, data, n = _spqr_encode(x, self.l1_centroids, self.l2_centroids, k)
+        if inverted:
+            rows, cols = cols, rows
+            shape = (self.vocab_size, n)
         else:
-        # ... while this partitions the array and sorts only the topk
-            unsorted_topk = fine_distances.argpartition(k, axis=2)[:, :, :k]
-            sorted_topk_idx = np.take_along_axis(fine_distances, unsorted_topk, axis=2).argsort(axis=2)
-            topk = np.take_along_axis(unsorted_topk, sorted_topk_idx, axis=2)  # m x n x k
+            shape = (n, self.vocab_size)
 
-        # build the representation
-        # the nested-for-loops way ...
+        return sparse.csr_matrix((data, (rows, cols)), shape=shape)
+    
+    @property
+    def prefix_length(self):
+        """(int): 
+            the number of nearest fine centroids to keep
+            when truncating the permutation; if None, the
+            whole permutation is used. Defaults to None.
         """
-        rows, cols, data = [], [], []
-        for subvector_index, sub_topk in enumerate(topk):  # 0 ...  m-1
-            for sample_index, nns in enumerate(sub_topk):  # 0 ... n
-                for importance, nn in enumerate(nns[::-1], start=1):  # 1 ... k
-                    rows.append(sample_index)
-                    cols.append(nn + self.f * subvector_index + self.f * self.m * coarse_codes[sample_index])
-                    data.append(importance)
-
-        rows = np.array(rows)
-        cols = np.array(cols)
-        data = np.array(data)
+        return self.permutation_length
+    
+    @prefix_length.setter
+    def prefix_length(self, l):
+        if (self.db.shape[1] > 0) and (l != self.permutation_length):
+            raise ValueError('Cannot change prefix_length of a populated index. Reset the index first.')
         
-        sparse_repr = csr_matrix((data, (rows, cols)), shape=(n, self.vocab_size))
-        """
-
-        # ... or the vectorized way (WARNING: heavy broadcasting ahead)
-        rows = np.arange(n).reshape(1, -1, 1)
-        m_shifts = np.arange(self.m).reshape(-1, 1, 1)
-        c_shifts = coarse_codes.reshape(1, -1, 1)
-        cols = topk + self.f * m_shifts + self.f * self.m * c_shifts
-        data = np.arange(k, 0, -1).reshape(1, 1, -1)
-        
-        rows, cols, data = np.broadcast_arrays(rows, cols, data)
-
-        rows = rows.flatten()
-        cols = cols.flatten()
-        data = data.flatten()
-
-        sparse_repr = csr_matrix((data, (rows, cols)), shape=(n, self.vocab_size))
-
-        return sparse_repr
-
-    def encode_parallel(self, x, permutation_length=None):
-
-        k = self.f if permutation_length is None else permutation_length
-
-        batch_size = int(math.ceil(len(x) / cpu_count()))
-        results = Parallel(n_jobs=-1, prefer='threads', require='sharedmem')(
-            delayed(_spqr_encode)(x[i:i+batch_size], self.l1_centroids, self.l2_centroids, k)
-            for i in range(0, len(x), batch_size)
-        )
-
-        results = vstack([
-            csr_matrix((data, (rows, cols)), shape=(n, self.vocab_size))
-            for rows, cols, data, n in results
-        ])
-
-        return results
+        self.permutation_length = l
