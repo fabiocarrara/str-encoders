63aa77559fa22d25f7a1d23e09fe4310518b110c
Fabio Carrara <fabio.carrara@isti.cnr.it>
2021-10-22 10:53:30 +0200

    added topk/bottomk and timer utils

--- utils.py
+++ utils.py
@@ -1,7 +1,8 @@
 import itertools
 from pathlib import Path
-import h5py
+import time
 
+import h5py
 import numpy as np
 import requests
 from tqdm import tqdm
@@ -65,9 +66,52 @@ def compute_recalls(true_neighbors, predicted_neighbors):
     # the one-liner
     return np.array([len(np.intersect1d(t, p)) / len(t) for t, p in zip(true_neighbors, predicted_neighbors)])
 
-# import dask.array as da
-# import dask_distance
-# def parallel_cdist(xa, xb, **kwargs):
-#     xa = da.from_array(xa, chunks=1000)
-#     xb = da.from_array(xb, chunks=1000)
-#     return dask_distance.cdist(xa, xb, **kwargs).compute()
+
+def bottomk_sorted(x, k, axis=0):
+    """ Returns sorted bottom-K indices along an axis.
+
+    Args:
+        x (ndarray): the source array.
+        k (int): number of bottom (smallest) elements to return.
+        axis (int): axis over which to extract the bottom-k values.
+
+    Returns:
+        ndarray: (N,K)-shaped array of indices.
+    """
+    if k >= x.shape[axis]:  # full sort needed
+        bottomk = x.argsort(axis=axis)
+    else:
+        unsorted_bottomk = x.argpartition(k, axis=axis).take(indices=range(k), axis=axis)
+        sorted_topk_idx = np.take_along_axis(x, unsorted_bottomk, axis=axis).argsort(axis=axis)
+        bottomk = np.take_along_axis(unsorted_bottomk, sorted_topk_idx, axis=axis)
+
+    return bottomk
+
+
+def topk_sorted(x, k, axis=0):
+    """ Returns sorted top-K indices along an axis.
+
+    Args:
+        x (ndarray): the source array.
+        k (int): number of top (largest) elements to return.
+        axis (int): axis over which to extract the top-k values.
+
+    Returns:
+        ndarray: (N,K)-shaped array of indices.
+    """
+    return bottomk_sorted(-x, k, axis=axis)
+
+
+class Timer:
+
+    def __init__(self, desc=None):
+        self.desc = desc
+
+    def __enter__(self):
+        self.start = time.time()
+
+    def __exit__(self, type, value, traceback):
+        self.stop = time.time()
+        self.duration = self.stop - self.start
+        prefix = (self.desc + ': ') if self.desc else ''
+        print(f"{prefix}{self.duration} s")
