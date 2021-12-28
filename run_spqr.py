11bb7e52f7a510de6cfb596053532eeddd4accfc
Fabio Carrara <fabio.carrara@isti.cnr.it>
2021-12-28 15:22:52 +0100

    refactored SPQR run script

--- run_spqr.py
+++ run_spqr.py
@@ -1,4 +1,5 @@
 import argparse
+from functools import partial
 import logging
 import math
 from pathlib import Path
@@ -9,9 +10,76 @@ from expman import Experiment
 import numpy as np
 import pandas as pd
 from tqdm import tqdm, trange
+from tqdm.contrib.logging import logging_redirect_tqdm
+
+tqdm = partial(tqdm, dynamic_ncols=True, leave=False)
+trange = partial(trange, dynamic_ncols=True, leave=False)
 
 from surrogate import SPQR
 from utils import get_ann_benchmark, nice_logspace, compute_recalls
+
+
+def load_or_train_index(x, C, M, F, trained_index_path, train_time_path):
+    if not Path(trained_index_path).exists() or args.force:
+        # train common index
+        logging.info('Training index ...')
+        d = x.shape[1]
+        train_time = time.time()
+        index = SPQR(d, n_coarse_centroids=C, n_subvectors=M, n_fine_centroids=F)
+        index.train(x)
+        train_time = time.time() - train_time
+        logging.info(f'Done in {train_time} s.')
+
+        # save trained index
+        logging.info(f'Saving trained index: {trained_index_path}')
+        with open(trained_index_path, 'wb') as f:
+            pickle.dump(index, f)
+
+        with open(train_time_path, 'w') as f:
+            f.write(str(train_time))
+    else:
+        logging.info(f'Reading pretrained index from: {trained_index_path}')
+        with open(trained_index_path, 'rb') as f:
+            index = pickle.load(f)
+            # index.parallel = True
+
+        with open(train_time_path, 'r') as f:
+            train_time = float(f.read().strip())
+    
+    return index, train_time
+
+
+def load_or_build_index(index, L, x, index_batch_size, force, built_index_path, build_time_path):
+    if not Path(built_index_path).exists() or force:
+        logging.info('Building index ...')
+        index.reset()
+        index.prefix_length = L
+        
+        n = len(x)
+        batch_size = n if index_batch_size is None else index_batch_size
+        build_time = time.time()
+        for i in trange(0, n, batch_size, desc='ADD'):
+            index.add(x[i:i+batch_size])
+        index.commit()
+        build_time = time.time() - build_time
+        logging.info(f'Done in {build_time} s.')
+
+        # save built index
+        logging.info(f'Saving built index: {built_index_path}')
+        with open(built_index_path, 'wb') as f:
+            pickle.dump(index, f)
+
+        with open(build_time_path, 'w') as f:
+            f.write(str(build_time))
+    else:
+        logging.info(f'Reading prebuilt index from: {built_index_path}')
+        with open(built_index_path, 'rb') as f:
+            index = pickle.load(f)
+
+        with open(build_time_path, 'r') as f:
+            build_time = float(f.read().strip())
+    
+    return index, build_time
 
 
 def main(args):
@@ -43,105 +111,84 @@ def main(args):
 
     trained_index_path = exp.path_to('empty_trained_index.pickle')
     train_time_path = exp.path_to('train_time.txt')
-    if not Path(trained_index_path).exists() or args.force:
-        # train common index
-        logging.info('Training index ...')
-        train_time = time.time()
-        index = SPQR(d, n_coarse_centroids=C, n_subvectors=M, n_fine_centroids=F)
-        index.train(x)
-        train_time = time.time() - train_time
-        logging.info(f'Done in {train_time} s.')
-
-        # save trained index
-        logging.info(f'Saving trained index: {trained_index_path}')
-        with open(trained_index_path, 'wb') as f:
-            pickle.dump(index, f)
-
-        with open(train_time_path, 'w') as f:
-            f.write(str(train_time))
-    else:
-        logging.info(f'Reading pretrained index from: {trained_index_path}')
-        with open(trained_index_path, 'rb') as f:
-            index = pickle.load(f)
-            # index.parallel = True
-
-        with open(train_time_path, 'r') as f:
-            train_time = float(f.read().strip())
+    
+    index, train_time = load_or_train_index(d, C, M, F, trained_index_path, train_time_path)
 
     metrics_path = exp.path_to('metrics.csv.gz')
-    metrics = pd.DataFrame() # pd.read_csv(metrics_path) if Path(metrics_path).exists() else pd.DataFrame()  #################### TOCHANGE
+    metrics = pd.read_csv(metrics_path) if Path(metrics_path).exists() else pd.DataFrame()
 
     ## SPQR
     logging.info('Building, Searching, and Evaluating SPQR index ...')
 
-    progress = tqdm(list(nice_logspace(min(F, 25))), desc='SPQR')
-    for L in progress:
-        progress.set_postfix({'prefix': L})
+    with logging_redirect_tqdm():
+        progress_L = tqdm(list(nice_logspace(min(F, 50))), desc='SPQR')
+        for L in progress_L:
+            progress_L.set_postfix({'prefix': L})
 
-        # skip if already done
-        if False: # not args.force:  #################### TOCHANGE
-            if not metrics.empty and (metrics['prefix_length'] == L).any():
-                logging.info(f'SKIPPING SPQR C={C} M={M} F={F} L={L}')
-                continue
+            logging.info(f'SPQR C={C} M={M} F={F} L={L}')
 
-        logging.info(f'SPQR C={C} M={M} F={F} L={L}')
+            # build index (lazily)
+            built_index_path = exp.path_to(f'built_index_perm_length_{L}.pickle')
+            build_time_path = exp.path_to(f'build_time_perm_length_{L}.txt')
+            is_built = False
 
-        # build index
-        built_index_path = exp.path_to(f'built_index_perm_length_{L}.pickle')
-        build_time_path = exp.path_to(f'build_time_perm_length_{L}.txt')
-        if not Path(built_index_path).exists() or True: # args.force:  #################### TOCHANGE
-            logging.info('Building index ...')
-            index.reset()
-            index.prefix_length = L
-            
-            batch_size = n if args.index_batch_size is None else args.index_batch_size
-            build_time = time.time()
-            for i in trange(0, n, batch_size, desc='ADD'):
-                index.add(x[i:i+batch_size])
-            index.commit()
-            build_time = time.time() - build_time
-            logging.info(f'Done in {build_time} s.')
+            search_time = -1
+            progress_P = tqdm(list(nice_logspace(min(C, 50))))
+            for nprobe in progress_P:
+                progress_P.set_postfix({'nprobe': nprobe})
 
-            # save built index
-            # logging.info(f'Saving built index: {built_index_path}')
-            # with open(built_index_path, 'wb') as f:
-            #     pickle.dump(index, f)
+                # skip if already done
+                if not args.force:
+                    if not metrics.empty and 'nprobe' in metrics.columns:
+                        sel = (metrics['prefix_length'] == L) & (metrics['nprobe'] == nprobe)
+                        if sel.any():
+                            search_time = metrics[sel].iloc[0]['search_time']
+                            logging.info(f'SKIPPING SPQR C={C} M={M} F={F} L={L} nprobe={nprobe}')
+                            continue
 
-            with open(build_time_path, 'w') as f:
-                f.write(str(build_time))
-        else:
-            logging.info(f'Reading prebuilt index from: {built_index_path}')
-            with open(built_index_path, 'rb') as f:
-                index = pickle.load(f)
+                if search_time > 5 * 60:  # 5 min
+                    logging.info(f'Skipping nprobes >= {nprobe}, search() would take too long (> 5min).')
+                    break
+                
+                if not is_built:  # build/load index (it's actually needed)
+                    index, build_time = load_or_build_index(index, L, x, args.index_batch_size, args.force, built_index_path, build_time_path)
+                    is_built = True
 
-            with open(build_time_path, 'r') as f:
-                build_time = float(f.read().strip())
+                index.nprobe = nprobe
 
-        # search and evaluate
-        batch_size = len(q) if args.search_batch_size is None else args.search_batch_size
-        search_time = time.time()
-        
-        logging.info(f'Searching index ...')
-        nns = []
-        for i in trange(0, len(q), batch_size, desc='SEARCH'):
-            _, nns_batch = index.search(q[i:i+batch_size], k=lim)
-            nns.append(nns_batch)
-        nns = np.vstack(nns)
-        search_time = time.time() - search_time
-        logging.info(f'Done in {search_time} s.')
+                # search and evaluate
+                batch_size = len(q) if args.search_batch_size is None else args.search_batch_size
+                batch_size = math.ceil(batch_size / nprobe)
 
-        new_metrics = pd.DataFrame([{
-            'index_type': 'spqr',
-            'build_time': build_time,
-            'num_entries': index.db.nnz,
-            'prefix_length': L,
-            'search_time': search_time,
-            'k': k,
-            'recall@k': compute_recalls(true_neighbors[:, :k], nns[:, :k]).mean(),
-        } for k in nice_logspace(lim)])
+                logging.info(f'Searching index ...')
+                search_time = time.time()
+                nns = []
+                for i in trange(0, len(q), batch_size, desc='SEARCH'):
+                    _, nns_batch = index.search(q[i:i+batch_size], k=lim)
+                    nns.append(nns_batch)
+                nns = np.vstack(nns)
+                search_time = time.time() - search_time
+                logging.info(f'Done in {search_time} s.')
 
-        metrics = pd.concat((metrics, new_metrics), ignore_index=True)
-        metrics.to_csv(metrics_path, index=False)
+                recalls = {k: compute_recalls(true_neighbors[:, :k], nns[:, :k]).mean() for k in nice_logspace(lim)}
+
+                new_metrics = pd.DataFrame([{
+                    'index_type': 'spqr',
+                    'train_time': train_time,
+                    'build_time': build_time,
+                    'num_entries': index.db.nnz,
+                    'prefix_length': L,
+                    'nprobe': nprobe,
+                    'search_time': search_time,
+                    'k': k,
+                    'recall@k': r_at_k,
+                } for k, r_at_k in recalls.items()])
+
+                metrics = pd.concat((metrics, new_metrics), ignore_index=True)
+                metrics.to_csv(metrics_path, index=False)
+
+                k = 100
+                logging.info(f'SPQR C={C} M={M} F={F} L={L} nprobe={nprobe}: R@{k}={recalls[k]:.3f}')
 
     logging.info('done')
     return
