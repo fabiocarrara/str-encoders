c79d44298d04ac528288e89cce217ad11dc01450
Fabio Carrara <fabio.carrara@isti.cnr.it>
2022-12-14 18:15:54 +0100

    refactored experiment run scripts

--- run.py
+++ run.py
@@ -10,144 +10,179 @@ import pandas as pd
 from tqdm import trange
 
 import surrogate
-from utils import get_ann_benchmark, nice_logspace, compute_recalls
+from utils import get_dataset, nice_logspace, compute_recalls
 
 
-def main(args):
-    ignore = ('data_root', 'exp_root', 'force', 'index_batch_size', 'search_batch_size')
-    exp = Experiment(args, root=args.exp_root, ignore=ignore)
-    print(exp)
-
-    # setup logging
+def configure_logging(log_path):
     stream_handler = logging.StreamHandler()
     stream_handler.setLevel(logging.INFO)
 
     logging.basicConfig(
         level=logging.DEBUG,
         format='%(asctime)s [%(levelname)s] %(module)s.%(funcName)s:%(lineno)d - %(message)s',
-        handlers=[logging.FileHandler(exp.path_to('log.txt'), mode='w'), stream_handler]
+        handlers=[logging.FileHandler(log_path, mode='a'), stream_handler]
     )
 
-    dataset = get_ann_benchmark(args.dataset, args.data_root)
-    
-    # load data in RAM
-    logging.info('Loading data ...')
-    x = dataset['train'][:]
-    q = dataset['test'][:]
-    n, d = x.shape
-    true_neighbors = dataset['neighbors'][:]
-    _, lim = true_neighbors.shape
 
-    index_type = args.index_type
-    index_params = {k: v for k, v in vars(args).items() if k not in ('dataset', 'index_type') + ignore}
+def load_or_train_index(x, train_params, trained_index_path, train_metrics_path, force=False):
+    if not Path(trained_index_path).exists() or force:
+        # create index
+        d = x.shape[1]
+        index_type = train_params.pop('index_type')
+        index = surrogate.index_factory(d, index_type, train_params)
 
-    # train index
-    trained_index_path = exp.path_to('empty_trained_index.pickle')
-    train_time_path = exp.path_to('train_time.txt')
-    if not Path(trained_index_path).exists() or args.force:
         # train index
-        logging.info('Training index ...')
+        index_repr = ' '.join([f'{k}={v}' for k, v in train_params.items()])
+        logging.info(f'Training index: {index_type}({index_repr})')
+
         train_time = time.time()
-        index = surrogate.index_factory(d, index_type, index_params)
         index.train(x)
         train_time = time.time() - train_time
         logging.info(f'Done in {train_time} s.')
 
         # save trained index
-        logging.info('Saving trained index ...')
+        logging.info(f'Saving trained index: {trained_index_path}')
         with open(trained_index_path, 'wb') as f:
             pickle.dump(index, f)
 
-        with open(train_time_path, 'w') as f:
-            f.write(str(train_time))
-        logging.info(f'Done: {trained_index_path}')
+        train_metrics = pd.DataFrame({
+            'train_time': train_time,
+        }, index=[0])
+        train_metrics.to_csv(train_metrics_path, index=False)
+
     else:
-        logging.info(f'Reading pretrained index from: {trained_index_path} ...')
+        logging.info(f'Reading pretrained index from: {trained_index_path}')
         with open(trained_index_path, 'rb') as f:
             index = pickle.load(f)
 
-        with open(train_time_path, 'r') as f:
-            train_time = float(f.read().strip())
+        train_metrics = pd.read_csv(train_metrics_path)
+    
+    return index, train_metrics
 
-    # build index
-    built_index_path = exp.path_to('built_index.pickle')
-    build_time_path = exp.path_to('build_time.txt')
-    if not Path(built_index_path).exists() or args.force:
-        # train index
+
+def load_or_build_index(index, x, build_params, built_index_path, build_metrics_path, index_batch_size, force):
+    if not Path(built_index_path).exists() or force:
         logging.info('Building index ...')
-        batch_size = n if args.index_batch_size is None else args.index_batch_size
+        index.reset()
+        
+        n = len(x)
+        batch_size = index_batch_size or n
         build_time = time.time()
-        for i in trange(0, len(x), batch_size, desc='ADD'):
-            index.add(x[i:i+batch_size])
+        for i in trange(0, n, batch_size, desc='ADD'):
+            index.add(x[i:i + batch_size])
         index.commit()
         build_time = time.time() - build_time
         logging.info(f'Done in {build_time} s.')
 
-        # save trained index
+        # save built index
         logging.info(f'Saving built index: {built_index_path}')
         with open(built_index_path, 'wb') as f:
             pickle.dump(index, f)
 
-        with open(build_time_path, 'w') as f:
-            f.write(str(build_time))
+        build_metrics = pd.DataFrame({
+            'build_time': build_time,
+            'build_batch_size': index_batch_size,
+            'index_density': index.density,
+            'num_entries': index.db.nnz,
+        }, index=[0])
+        build_metrics.to_csv(build_metrics_path, index=False)
+
     else:
         logging.info(f'Reading prebuilt index from: {built_index_path}')
         with open(built_index_path, 'rb') as f:
             index = pickle.load(f)
 
-        with open(build_time_path, 'r') as f:
-            build_time = float(f.read().strip())
+        build_metrics = pd.read_csv(build_metrics_path)
+    
+    return index, build_metrics
 
-    metrics_path = exp.path_to('metrics.csv.gz')
-    if Path(metrics_path).exists() and not args.force:
-        logging.info(f'Skipping: {metrics_path} found.')
-        return
+
+def main(args):
+    all_params = vars(args)
+
+    train_params = all_params.pop('train_params') + ('index_type',)
+    build_params = all_params.pop('build_params')
+    query_params = all_params.pop('query_params')
+    index_params = train_params + build_params + query_params
+
+    train_params  = {k: v for k, v in all_params.items() if k     in train_params}
+    build_params  = {k: v for k, v in all_params.items() if k     in build_params}
+    query_params  = {k: v for k, v in all_params.items() if k     in query_params}
+    common_params = {k: v for k, v in all_params.items() if k not in index_params}
+
+    root_params = dict(**common_params, **train_params)
+    ignore = common_params.keys() - {'dataset'}  # ignore all common params but 'dataset'
+    exp_train = Experiment(root_params, root=args.exp_root, ignore=ignore)
+    print(exp_train)
+
+    # setup logging
+    log_path = exp_train.path_to('log.txt')
+    configure_logging(log_path)
+    
+    # load data in RAM
+    dataset = get_dataset(args.dataset, args.data_root)
+    
+    logging.info(f'Loading data: {args.dataset}.hdf5')
+    x = dataset['train'][:]
+    q = dataset['test'][:10]
+    n, d = x.shape
+    true_neighbors = dataset['neighbors'][:10]
+    _, lim = true_neighbors.shape
+
+    # train index
+    trained_index_path = exp_train.path_to('empty_trained_index.pickle')
+    train_metrics_path = exp_train.path_to('train_metrics.csv')
+    index, train_metrics = load_or_train_index(x, train_params, trained_index_path, train_metrics_path)
+
+    # build index
+    exp_build = Experiment(build_params, root=exp_train.path) if build_params else exp_train
+    print(exp_build)
+    built_index_path = exp_build.path_to('built_index.pickle')
+    build_metrics_path = exp_build.path_to('build_metrics.csv')
+    load_or_build_index(index, x, build_params, built_index_path, build_metrics_path, args.index_batch_size, args.force)
 
     # search and evaluate
-    logging.info(f'Index density: {index.density:.2%}')
-    batch_size = len(q) if args.search_batch_size is None else args.search_batch_size
+    exp_search = Experiment(query_params, root=exp_build.path) if query_params else exp_build
+    print(exp_search)
+
+    search_metrics_path = exp_search.path_to('search_metrics.csv')
+    if Path(search_metrics_path).exists() and not args.force:
+        logging.info('Skipping run.')
+        return
+
+    batch_size = args.search_batch_size or len(q)
     logging.info('Searching and Evaluating index ...')
     search_time = time.time()
     nns = []
+    search_cost = 0
     for i in trange(0, len(q), batch_size, desc='SEARCH'):
-        _, nns_batch = index.search(q[i:i+batch_size], k=lim)
+        _, nns_batch, cost = index.search(q[i:i + batch_size], k=lim, return_cost=True, **query_params)
         nns.append(nns_batch)
+        search_cost += cost
     nns = np.vstack(nns)
     search_time = time.time() - search_time
 
-    metrics = pd.DataFrame([{
-        'build_time': build_time,
-        'index_density': index.density,
-        'num_entries': index.db.nnz,
-        'search_time': search_time,
-        'k': k,
-        'recall@k': compute_recalls(true_neighbors[:, :k], nns[:, :k]).mean(),
-    } for k in nice_logspace(lim)])
-
-    metrics.to_csv(metrics_path, index=False)
+    search_metrics = []
+    for k in nice_logspace(lim):
+        recalls = compute_recalls(true_neighbors[:, :k], nns[:, :k])
+        search_metrics.append({
+            'k': k,
+            'recall@k.mean': recalls.mean(),
+            'recall@k.std': recalls.std(),
+        })
+    
+    search_metrics = pd.DataFrame(search_metrics)
+    search_metrics['search_time'] = search_time
+    search_metrics['search_cost'] = search_cost
+    search_metrics.to_csv(search_metrics_path, index=False)
 
     logging.info(f'Done in {search_time} s.')
-    return
 
-
-ANN_DATASETS = (
-    'deep-image-96-angular',
-    'fashion-mnist-784-euclidean',
-    'gist-960-euclidean',
-    'glove-25-angular',
-    'glove-50-angular',
-    'glove-100-angular',
-    'glove-200-angular',
-    'kosarak-jaccard',
-    'mnist-784-euclidean',
-    'nytimes-256-angular',
-    'sift-128-euclidean',
-    'lastfm-64-dot',
-)
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Run Surrogate Text Representation Experiments')
-    parser.add_argument('dataset', choices=ANN_DATASETS, help='dataset from ann-benchmarks.com')
+    parser = argparse.ArgumentParser(description='Run Surrogate Text Representation Experiments',
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('dataset', help='dataset from ann-benchmarks.com')
 
     parser.add_argument('--data-root', default='data/', help='where to store downloaded data')
     parser.add_argument('--exp-root', default='runs/', help='where to store results')
@@ -155,9 +190,8 @@ if __name__ == "__main__":
     parser.add_argument('--force', default=False, action='store_true', help='force index training')
     parser.add_argument('-b', '--index-batch-size', type=int, default=None, help='index data in batches with this size')
     parser.add_argument('-B', '--search-batch-size', type=int, default=None, help='search data in batches with this size')
-    args, rest = parser.parse_known_args()
+    parser.add_argument('-t', '--search-timeout', type=int, default=1000, help='stop parameter search when search time (in seconds) is over this value')
 
-    index_parser = surrogate.argparser()
-    args = index_parser.parse_args(rest, namespace=args)
-
+    parser = surrogate.add_index_argparser(parser)
+    args = parser.parse_args()
     main(args)
--- surrogate/__init__.py
+++ surrogate/__init__.py
@@ -31,54 +31,92 @@ def index_factory(d, index_type, index_params):
     raise NotImplementedError(f'{index_type} not implemented')
 
 
-def argparser():
+def add_index_argparser(parser):
+    subparsers = parser.add_subparsers(dest='index_type', help='STR index type. Can be one of:', metavar='index', required=True)
+    common = dict(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-    parser = argparse.ArgumentParser(description='Surrogate Text Index Arguments',
-                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    subparsers = parser.add_subparsers(dest='index_type')
-
-    deep_perm_parser = subparsers.add_parser('deep-perm', help='Deep Permutation STR Index')
+    deep_perm_parser = subparsers.add_parser('deep-perm', help='Deep Permutation', **common)
     deep_perm_parser.add_argument('-c', '--use-centroids', action='store_true', default=False, help='Find Pivots with k-Means.')
-    deep_perm_parser.add_argument('-L', '--permutation-length', type=int, default=None, help='length of the permutation prefix (None for full permutation)')
     deep_perm_parser.add_argument('-C', '--rectify-negatives', action='store_true', default=False, help='Apply CReLU trasformation.')
+    deep_perm_parser.add_argument('-L', '--permutation-length', type=int, default=None, help='length of the permutation prefix (None for full permutation)')
+    deep_perm_parser.set_defaults(
+        train_params=('use_centroids',),
+        build_params=('rectify_negatives', 'permutation_length'),
+        query_params=()
+    )
 
-    ivf_deep_perm_parser = subparsers.add_parser('ivf-deep-perm', help='Chunked Deep Permutation STR Index')
+    ivf_deep_perm_parser = subparsers.add_parser('ivf-deep-perm', help='Chunked Deep Permutation', **common)
     ivf_deep_perm_parser.add_argument('-c', '--n-coarse-centroids', type=int, default=None, help='no of coarse centroids')
-    ivf_deep_perm_parser.add_argument('-C', '--rectify-negatives', action='store_true', default=False, help='Apply CReLU trasformation.')
     ivf_deep_perm_parser.add_argument('-n', '--l2-normalize', action='store_true', default=False, help='L2-normalize vectors before processing.')
+    ivf_deep_perm_parser.add_argument('-C', '--rectify-negatives', action='store_true', default=False, help='Apply CReLU trasformation.')
     ivf_deep_perm_parser.add_argument('-L', '--permutation-length', type=int, default=None, help='length of the permutation prefix (None for full permutation)')
+    ivf_deep_perm_parser.add_argument('-p', '--nprobe', type=int, default=None, help='how many partitions to visit at query time')
+    ivf_deep_perm_parser.set_defaults(
+        train_params=('n_coarse_centroids', 'l2_normalize'),
+        build_params=('rectify_negatives', 'permutation_length'),
+        query_params=('nprobe',)
+    )
 
-    thrsq_parser = subparsers.add_parser('thr-sq', help='Threshold Scalar Quantization STR Index')
+    thrsq_parser = subparsers.add_parser('thr-sq', help='Threshold Scalar Quantization', **common)
+    thrsq_parser.add_argument('-n', '--l2-normalize', action='store_true', default=False, help='L2-normalize vectors before processing.')
+    thrsq_parser.add_argument('-C', '--rectify-negatives', action='store_true', default=False, help='Apply CReLU trasformation.')
+    thrsq_parser.add_argument('-m', '--subtract-mean', action='store_true', default=False, help='Compute and subtract mean database vector.')
+    thrsq_parser.add_argument('-r', '--rotation-matrix', type=int, default=None, help='seed for generating a random orthogonal matrix to apply to vectors; if omitted, no transformation is applied.')
     thrsq_parser.add_argument('-Q', '--threshold-percentile', type=int, default=75, help='Controls how many values are discarded when encoding. Must be between 1 and 99 inclusive.')
     thrsq_parser.add_argument('-s', '--sq-factor', type=float, default=1000, help='Controls the quality of the scalar quantization.')
-    thrsq_parser.add_argument('-C', '--rectify-negatives', action='store_true', default=False, help='Apply CReLU trasformation.')
-    thrsq_parser.add_argument('-n', '--l2-normalize', action='store_true', default=False, help='L2-normalize vectors before processing.')
+    thrsq_parser.set_defaults(
+        train_params=('l2_normalize', 'rectify_negatives', 'subtract_mean', 'rotation_matrix', 'threshold_percentile'),
+        build_params=('sq_factor',),
+        query_params=()
+    )
 
-    ivfthrsq_parser = subparsers.add_parser('ivf-thr-sq', help='Residual Chunked Threshold Scalar Quantization STR Index')
+    ivfthrsq_parser = subparsers.add_parser('ivf-thr-sq', help='Residual Chunked Threshold Scalar Quantization', **common)
+    ivfthrsq_parser.add_argument('-n', '--l2-normalize', action='store_true', default=False, help='L2-normalize vectors before processing.')
+    ivfthrsq_parser.add_argument('-C', '--rectify-negatives', action='store_true', default=False, help='Apply CReLU trasformation.')
     ivfthrsq_parser.add_argument('-c', '--n-coarse-centroids', type=int, default=None, help='no of coarse centroids')
     ivfthrsq_parser.add_argument('-m', '--n-subvectors', type=int, default=None, help='no of subvectors')
     ivfthrsq_parser.add_argument('-Q', '--threshold-percentile', type=int, default=75, help='Controls how many values are discarded when encoding. Must be between 1 and 99 inclusive.')
     ivfthrsq_parser.add_argument('-s', '--sq-factor', type=float, default=1000, help='Controls the quality of the scalar quantization.')
-    ivfthrsq_parser.add_argument('-C', '--rectify-negatives', action='store_true', default=False, help='Apply CReLU trasformation.')
-    ivfthrsq_parser.add_argument('-n', '--l2-normalize', action='store_true', default=False, help='L2-normalize vectors before processing.')
+    ivfthrsq_parser.add_argument('-p', '--nprobe', type=int, default=None, help='how many partitions to visit at query time')
+    ivfthrsq_parser.set_defaults(
+        train_params=('l2_normalize', 'rectify_negatives', 'n_coarse_centroids', 'n_subvectors', 'threshold_percentile'),
+        build_params=('sq_factor',),
+        query_params=('nprobe',)
+    )
 
-    topksq_parser = subparsers.add_parser('topk-sq', help='TopK Scalar Quantization STR Index')
-    topksq_parser.add_argument('-k', '--keep', type=float, default=0.25, help='Controls how many values are discarded when encoding. Must be between 0.0 and 1.0 inclusive.')
-    topksq_parser.add_argument('-s', '--sq-factor', type=float, default=1000, help='Controls the quality of the scalar quantization.')
-    topksq_parser.add_argument('-C', '--rectify-negatives', action='store_true', default=False, help='Apply CReLU trasformation.')
+    topksq_parser = subparsers.add_parser('topk-sq', help='TopK Scalar Quantization', **common)
     topksq_parser.add_argument('-n', '--l2-normalize', action='store_true', default=False, help='L2-normalize vectors before processing.')
+    topksq_parser.add_argument('-C', '--rectify-negatives', action='store_true', default=False, help='Apply CReLU trasformation.')
+    topksq_parser.add_argument('-s', '--sq-factor', type=float, default=1000, help='Controls the quality of the scalar quantization.')
+    topksq_parser.add_argument('-k', '--keep', type=float, default=0.25, help='Controls how many values are discarded when encoding. Must be between 0.0 and 1.0 inclusive.')
+    topksq_parser.set_defaults(
+        train_params=(),
+        build_params=('l2_normalize', 'rectify_negatives', 'sq_factor', 'keep'),
+        query_params=()
+    )
 
-    ivftopksq_parser = subparsers.add_parser('ivf-topk-sq', help='Chunked TopK Scalar Quantization STR Index')
-    ivftopksq_parser.add_argument('-c', '--n-coarse-centroids', type=int, default=None, help='no of coarse centroids')
-    ivftopksq_parser.add_argument('-m', '--n-subvectors', type=int, default=None, help='no of subvectors')
-    ivftopksq_parser.add_argument('-k', '--keep', type=float, default=0.25, help='Controls how many values are discarded when encoding. Must be between 0.0 and 1.0 inclusive.')
-    ivftopksq_parser.add_argument('-s', '--sq-factor', type=float, default=1000, help='Controls the quality of the scalar quantization.')
-    ivftopksq_parser.add_argument('-C', '--rectify-negatives', action='store_true', default=False, help='Apply CReLU trasformation.')
+    ivftopksq_parser = subparsers.add_parser('ivf-topk-sq', help='Chunked TopK Scalar Quantization', **common)
     ivftopksq_parser.add_argument('-n', '--l2-normalize', action='store_true', default=False, help='L2-normalize vectors before processing.')
+    ivftopksq_parser.add_argument('-C', '--rectify-negatives', action='store_true', default=False, help='Apply CReLU trasformation.')
+    ivftopksq_parser.add_argument('-c', '--n-coarse-centroids', type=int, default=512, help='no of coarse centroids')
+    ivftopksq_parser.add_argument('-m', '--n-subvectors', type=int, default=1, help='no of subvectors')
+    ivftopksq_parser.add_argument('-s', '--sq-factor', type=float, default=1000, help='Controls the quality of the scalar quantization.')
+    ivftopksq_parser.add_argument('-k', '--keep', type=float, default=0.25, help='Controls how many values are discarded when encoding. Must be between 0.0 and 1.0 inclusive.')
+    ivftopksq_parser.add_argument('-p', '--nprobe', type=int, default=1, help='how many partitions to visit at query time')
+    ivftopksq_parser.set_defaults(
+        train_params=('l2_normalize', 'n_coarse_centroids', 'n_subvectors'),
+        build_params=('rectify_negatives', 'sq_factor', 'keep'),
+        query_params=('nprobe',)
+    )
 
-    spqr_parser = subparsers.add_parser('spqr', help='SPQR STR Index')
+    spqr_parser = subparsers.add_parser('spqr', help='SPQR', **common)  # TODO remove
     spqr_parser.add_argument('-c', '--n-coarse-centroids', type=int, default=None, help='no of coarse centroids')
     spqr_parser.add_argument('-m', '--n-subvectors', type=int, default=None, help='no of subvectors for PQ')
     spqr_parser.add_argument('-f', '--n-fine-centroids', type=int, default=2**8, help='no of fine centroids')
-
+    spqr_parser.set_defaults(
+        train_params=('n_coarse_centroids', 'n_subvectors', 'n_fine_centroids'),
+        build_params=(),
+        query_params=()
+    )
+    
     return parser
